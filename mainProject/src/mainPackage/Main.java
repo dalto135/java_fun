162d59aad1532acc082457217ceea5d40bbda4f0
dalto135 <67397294+dalto135@users.noreply.github.com>
2026-01-08 22:18:49 -0500

    renamed things

---
+++ mainProject/src/mainPackage/Main.java
@@ -0,0 +1,15 @@
+package mainPackage;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean truthPoop = 5 > 3;
+        boolean falsehoodPoop = 3 > 5;
+
+        if (truthPoop && falsehoodPoop) {
+            System.out.println("both true");
+        }
+        else {
+            System.out.println("better luck next time fucko");
+        }
+    }
+}
