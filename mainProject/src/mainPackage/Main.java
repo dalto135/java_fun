4914225866fed4dffc709fc07293fa375a8ec1e8
dalto135 <67397294+dalto135@users.noreply.github.com>
2026-01-14 21:41:50 -0500

    created separate method

--- mainProject/src/mainPackage/Main.java
+++ mainProject/src/mainPackage/Main.java
@@ -2,14 +2,24 @@ package mainPackage;
 
 public class Main {
     public static void main(String[] args) {
+        String statement = helloMethod();
+
+        System.out.println(statement);
+    }
+
+    public static String helloMethod() {
         boolean truthPoop = 5 > 3;
         boolean falsehoodPoop = 3 > 5;
 
+        String statement;
+
         if (truthPoop && falsehoodPoop) {
-            System.out.println("both true");
+            statement = "both true";
         }
         else {
-            System.out.println("better luck next time fucko");
+            statement = "better luck next time fucko";
         }
+
+        return statement;
     }
 }
